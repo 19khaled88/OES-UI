9dcd1e4ab1629471a51125620e427dc3b074dc64
19khaled88 <kkhaled88hasan@gmail.com>
2023-09-26 16:58:03 +0600

    work on side bar items

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -3,83 +3,63 @@ import Sider from "antd/es/layout/Sider";
 import { theme, Menu } from "antd";
 import type { MenuProps } from "antd";
 import React, { useState } from "react";
-import {
-  DesktopOutlined,
-  FileOutlined,
-  PieChartOutlined,
-  TeamOutlined,
-  UserOutlined,
-} from "@ant-design/icons";
+import OES from "../../../public/OES.png";
+
 import DynamicItems from "@/constants/dynamicItems";
 import { USER_ROLE } from "@/constants/role";
-type MenuItem = Required<MenuProps>["items"][number];
-const role = USER_ROLE.STUDENT
-function getItem(
-  label: React.ReactNode,
-  key: React.Key,
-  icon?: React.ReactNode,
-  children?: MenuItem[]
-): MenuItem {
-  return {
-    key,
-    icon,
-    children,
-    label,
-  } as MenuItem;
-}
+import Image from "next/image";
 
-const items: MenuItem[] = [
-  getItem("Option 1", "1", <PieChartOutlined />),
-  getItem("Option 2", "2", <DesktopOutlined />),
-  getItem("User", "sub1", <UserOutlined />, [
-    getItem("Tom", "3"),
-    getItem("Bill", "4"),
-    getItem("Alex", "5"),
-  ]),
-  getItem("Team", "sub2", <TeamOutlined />, [
-    getItem("Team 1", "6"),
-    getItem("Team 2", "8"),
-  ]),
-  getItem("Files", "9", <FileOutlined />),
-];
+const role = USER_ROLE.STUDENT;
 
 const Sidebar = () => {
-  const [collapsed, setCollapsed] = useState(false);
+	const [collapsed, setCollapsed] = useState(false);
+	return (
+		<Sider
+			width={280}
+			style={{
+				overflow: "auto",
+				height: "100vh",
+				position: "sticky",
+				left: 0,
+				top: 0,
+				bottom: 0,
+			}}
+			collapsible
+			collapsed={collapsed}
+			onCollapse={(value) => setCollapsed(value)}
+		>
+			<div
+				style={{
+					color: "white",
+					fontSize: "1.2rem",
+					textAlign: "center",
+					fontWeight: "bold",
+					marginBottom: "1rem",
+					display: "flex",
 
-  return (
-    <Sider
-      width={280}
-      style={{
-        overflow: "auto",
-        height: "100vh",
-        position: "sticky",
-        left: 0,
-        top: 0,
-        bottom: 0,
-      }}
-      collapsible
-      collapsed={collapsed}
-      onCollapse={(value) => setCollapsed(value)}
-    >
-      <div
-        style={{
-          color: "white",
-          fontSize: "1.5rem",
-          textAlign: "center",
-          fontWeight: "bold",
-          marginBottom: "1rem",
-        }}
-      >
-        Online Eduction system
-      </div>
-      <Menu
-        theme="dark"
-        defaultSelectedKeys={["1"]}
-        mode="inline"
-        items={DynamicItems(role)}
-      />
-    </Sider>
-  );
+					// flexWrap: "wrap",
+					flexDirection: "row",
+					justifyContent: "space-evenly",
+					paddingTop: "10px",
+				}}
+			>
+				{collapsed === true ? (
+					<Image src={OES} width={30} height={30} alt="No Image" />
+				) : (
+					<>
+						<Image src={OES} width={30} height={30} alt="No Image" /> Education
+						Digitalization
+					</>
+				)}
+			</div>
+			<Menu
+				theme="dark"
+				defaultSelectedKeys={["1"]}
+				mode="inline"
+				items={DynamicItems(role)}
+			/>
+		</Sider>
+	);
 };
 
 export default Sidebar;
--- src/constants/dynamicItems.tsx
+++ src/constants/dynamicItems.tsx
@@ -1,7 +1,221 @@
+import { MenuProps } from "antd";
 import React from "react";
+import {
+  FileTextFilled,
+	CreditCardOutlined,
+	ThunderboltOutlined,
+	ScheduleOutlined,
+	UserOutlined,
+	ProfileFilled,
+	LockOutlined,
+	TableOutlined,
+	AppstoreOutlined,
+} from "@ant-design/icons";
+import Link from "next/link";
+import { USER_ROLE } from "./role";
 
 const DynamicItems = (role: string) => {
-  return;
+	const defaultItems: MenuProps["items"] = [
+		{
+			label: "Profile",
+			key: "profile",
+			icon: <UserOutlined />,
+			children: [
+				{
+					label: <Link href={`/${role}/account-profile`}>Account Profile</Link>,
+					key: `/${role}/account-profile`,
+					icon: <ProfileFilled />,
+				},
+				{
+					label: <Link href={`/${role}/change-password`}>Change Password</Link>,
+					key: `/${role}/change-password`,
+					icon: <LockOutlined />,
+				},
+			],
+		},
+	];
+
+	const commonAdminItems: MenuProps["items"] = [
+		{
+			label: <Link href={`/${role}/manage-students`}>Manage-Students</Link>,
+			icon: <TableOutlined />,
+			key: [`/${role}/manage-students`],
+		},
+		{
+			label: <Link href={`/${role}/manage-faculty`}>Manage-Faculty</Link>,
+			icon: <TableOutlined />,
+			key: [`/${role}/manage-faculty`],
+		},
+	];
+
+	const adminItems: MenuProps["items"] = [
+		...defaultItems,
+		...commonAdminItems,
+		{
+			label: "Manage Academic",
+			key: "manage-acacdemic",
+			icon: <TableOutlined />,
+			children: [
+				{
+					label: <Link href={`/${role}/academic/facultiy`}>Faculties</Link>,
+					key: `/${role}/academic/faculty`,
+				},
+				{
+					label: (
+						<Link href={`/${role}/academic/departments`}>Departments</Link>
+					),
+					key: `/${role}/academic/department`,
+				},
+				{
+					label: <Link href={`/${role}/academic/semester`}>Semester</Link>,
+					key: `/${role}/academic/semester`,
+				},
+			],
+		},
+		{
+			label: "Management",
+			key: "management",
+			icon: <AppstoreOutlined />,
+			children: [
+				{
+					label: <Link href={`/${role}/department`}>Department</Link>,
+					key: `/${role}/department`,
+				},
+				{
+					label: <Link href={`/${role}/building`}>Building</Link>,
+					key: `/${role}/building`,
+				},
+				{
+					label: <Link href={`/${role}/room`}>Room</Link>,
+					key: `/${role}/room`,
+				},
+				{
+					label: <Link href={`/${role}/course`}>Course</Link>,
+					key: `/${role}/course`,
+				},
+				{
+					label: (
+						<Link href={`/${role}/semester-registration`}>
+							Semester registration
+						</Link>
+					),
+					key: `/${role}/semester-registration`,
+				},
+				{
+					label: <Link href={`/${role}/offered-course`}>Offered courses</Link>,
+					key: `/${role}/offered-course`,
+				},
+				{
+					label: (
+						<Link href={`/${role}/offered-course-section`}>
+							Course sections
+						</Link>
+					),
+					key: `/${role}/offered-course-section`,
+				},
+				{
+					label: (
+						<Link href={`/${role}/offered-course-schedule`}>
+							Course schedules
+						</Link>
+					),
+					key: `/${role}/offered-course-schedule`,
+				},
+			],
+		},
+	];
+
+	const superAdminItems: MenuProps["items"] = [
+		...defaultItems,
+		...commonAdminItems,
+		{
+			label: <Link href={`/${role}/admin`}>Manage Admin</Link>,
+			key: `/${role}/admin`,
+			icon: <TableOutlined />,
+		},
+		{
+			label: <Link href={`/${role}/user`}>Manage User</Link>,
+			key: `/${role}/user`,
+			icon: <TableOutlined />,
+		},
+		{
+			label: "Manage permission",
+			key: "manage-permission",
+			icon: <AppstoreOutlined />,
+			children: [
+				{
+					label: <Link href={`/${role}/permission`}>View permission</Link>,
+					key: `/${role}/permission`,
+				},
+			],
+		},
+		{
+			label: "Management",
+			key: "management",
+			icon: <AppstoreOutlined />,
+			children: [
+				{
+					label: <Link href={`/${role}/department`}>Department</Link>,
+					key: `/${role}/department`,
+				},
+			],
+		},
+	];
+
+	const facultyItems: MenuProps["items"] = [
+		...defaultItems,
+		{
+			label: <Link href={`/${role}/courses`}>Courses</Link>,
+			key: `/${role}/courses`,
+			icon: <TableOutlined />,
+		},
+	];
+  
+	const studentItems: MenuProps["items"] = [
+		...defaultItems,
+		{
+			label: <Link href={`/${role}/courses`}>Courses</Link>,
+			key: `/${role}/courses`,
+			icon: <TableOutlined />,
+		},
+		{
+			label: <Link href={`/${role}/courses/schedule`}>Courses schedules</Link>,
+			key: `/${role}/courses/schedule`,
+			icon: <ScheduleOutlined />,
+		},
+		{
+			label: <Link href={`/${role}/courses/schedule`}>Courses schedules</Link>,
+			key: `/${role}/courses/schedule`,
+			icon: <ScheduleOutlined />,
+		},
+		{
+			label: <Link href={`/${role}/registration`}>Registration</Link>,
+			key: `/${role}/registration`,
+			icon: <ThunderboltOutlined />,
+		},
+		{
+			label: <Link href={`/${role}/academic-report`}>Academic Report</Link>,
+			key: `/${role}/academic-report`,
+			icon: <CreditCardOutlined />,
+		},
+		{
+			label: <Link href={`/${role}/payment`}>Payment</Link>,
+			key: `/${role}/payment`,
+			icon: <CreditCardOutlined />,
+		},
+	];
+
+	if(role === USER_ROLE.SUPER_ADMIN){
+    return superAdminItems
+  }else if(role === USER_ROLE.ADMIN){
+    return adminItems
+  }else if(role === USER_ROLE.FACULTY){
+    return facultyItems
+  }else if(role === USER_ROLE.STUDENT){
+    return studentItems
+  }else {
+    return commonAdminItems
+  }
 };
 
 export default DynamicItems;
